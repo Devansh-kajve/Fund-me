acfe1546babe8a3565dedd43696c0c6d4f7dfa6d
Devansh-kajve <devanshkajve123@gmail.com>
2022-12-28 18:45:16 +0530

    first commit

---
+++ test/staging/fundme.staging.test.js
@@ -0,0 +1,16 @@
+const { deployments, ethers, getNamedAccounts, network } = require("hardhat")
+const { assert, expect } = require("chai")
+const { developmentChains } = require("../../helper-hardhat-config")
+
+developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("FundMe", async function () {
+          let fundMe
+          let deployer
+          let mockV3Aggregator
+          const sendValue = ethers.utils.parseEther("1")
+          beforeEach(async function () {
+              deployer = (await getNamedAccounts()).deployer
+              fundMe = await ethers.getContract("FundMe", deployer)
+          })
+      })
